f2718fe169cd4f776366229554081d393df97289
oleg.pronchenko <traeop@gmail.com>
2026-02-26 12:41:20 +0200

    chore: configure GitHub Actions for deployment to Pages
    Set up a workflow for deploying the application to GitHub Pages
    upon pushes to the main or master branches. This includes
    installation of dependencies, building the project, and
    uploading artifacts.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS
+    ? `/${process.env.GITHUB_REPOSITORY?.split("/")[1] ?? ""}/`
+    : "/",
   plugins: [react(), tailwindcss()],
 });
